d4d96bbc1ea8ed9d636f15a485c83fb1baa546d1
LeAustinHan <314456527@qq.com>
2019-11-28 17:44:17 +0800

    add project files

---
+++ LBOCFirstDemo/LBOCFirstDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LBOCFirstDemo
+//
+//  Created by Han Jize on 2019/11/28.
+//  Copyright © 2019 LBOC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LBOCFirstDemo/LBOCFirstDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LBOCFirstDemo
+//
+//  Created by Han Jize on 2019/11/28.
+//  Copyright © 2019 LBOC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
